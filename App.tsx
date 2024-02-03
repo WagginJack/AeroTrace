ce5d683d345d67628b08879d5c045345c3843bd1
Ronald Heminway <ronald.heminway@ku.edu>
2024-02-02 23:55:14 -0600

    initial commit

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, Image, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./AeroTrace_Banner.png')} style={styles.banner} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Velocity</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Altitude</Text>
+        </TouchableOpacity>
+        {/* Add more buttons as needed */}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  banner: {
+    width: '100%',
+    height: 100, // Adjust the height as needed
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginTop: 20, 
+  },
+  button: {
+    backgroundColor: "#71dc71", 
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    width: 200,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-AeroTrace-AeroTraceTests/Pods-AeroTrace-AeroTraceTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AeroTrace_AeroTraceTests : NSObject
+@end
+@implementation PodsDummy_Pods_AeroTrace_AeroTraceTests
+@end
