28214ac94124262a6774605b61dac8dc145d66bb
WagginJack <ronald.heminway@ku.edu>
2024-02-08 17:42:39 -0600

    added packages

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
     <View style={[styles.container,
     
       { 
-        marginTop: Platform.OS === "ios" ? 40 : 0 
+        marginTop: Platform.OS === "ios" ? 50 : 10 
       }
     
     ]}>
