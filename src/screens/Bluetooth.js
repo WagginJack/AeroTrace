caea1c4e5c20d75e1611595b25eeda3053a52e8b
Ronald Heminway <ronald.heminway@ku.edu>
2024-04-26 14:08:28 -0500

    added event listener

--- src/screens/Bluetooth.js
+++ src/screens/Bluetooth.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { View, Text, Button, FlatList, NativeEventEmitter} from 'react-native';
 import BleManager from 'react-native-ble-manager';
+
+const bleManagerEmitter = new NativeEventEmitter(BleManager);
 
 let BLEid = ''
 var NotificationOn = false;
